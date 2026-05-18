19dfc074f49821baef02142b678bee6451a032b2
Arteeemis <dipper3108@gmail.com>
2026-05-18 04:14:11 +0300

    add adaptivity for header

--- education_cms_frontend/src/app/layout/header/header.component.ts
+++ education_cms_frontend/src/app/layout/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, signal, HostListener } from '@angular/core';
+import { Router, NavigationEnd, RouterLink } from '@angular/router';
+import { filter } from 'rxjs';
 import { LayoutService } from '@core/services/layout.service';
 import { HeaderLinksBlockComponent } from '@features/page/blocks/header-links-block/header-links-block.component';
 
@@ -12,5 +13,29 @@ import { HeaderLinksBlockComponent } from '@features/page/blocks/header-links-bl
 })
 export class HeaderComponent {
   private readonly layout = inject(LayoutService);
+  private readonly router = inject(Router);
+
   readonly menu = this.layout.headerLinks;
+  readonly mobileOpen = signal(false);
+
+  constructor() {
+    // Закрываем меню при переходе по ссылке
+    this.router.events
+      .pipe(filter((e) => e instanceof NavigationEnd))
+      .subscribe(() => this.mobileOpen.set(false));
+  }
+
+  toggleMobile(): void {
+    this.mobileOpen.update((v) => !v);
+  }
+
+  closeMobile(): void {
+    this.mobileOpen.set(false);
+  }
+
+  // Закрываем меню по Escape
+  @HostListener('document:keydown.escape')
+  onEscape(): void {
+    if (this.mobileOpen()) this.closeMobile();
+  }
 }
